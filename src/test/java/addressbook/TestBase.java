9acd5706acc2a28f46c46d78a9414988a72f8ae7
Alex Lobov <alobov@issart.com>
2022-07-05 18:02:37 +0700

    Перенес все в appmanager кроме setup и teardown

--- src/test/java/addressbook/TestBase.java
+++ src/test/java/addressbook/TestBase.java
@@ -1,20 +1,11 @@
 package addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 public class TestBase extends AppManager {
-
-    ChromeDriver driver;
-    String addressBookUrl = "http://localhost/addressbook/";
-    String userName = "admin";
-    String password = "secret";
 
 
     @BeforeMethod
@@ -22,76 +13,9 @@ public class TestBase extends AppManager {
         init();
     }
 
-    private void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        login(userName, password);
-    }
-
-    public void login(String userName, String password) {
-        driver.get(addressBookUrl);
-        driver.findElement(By.name("user")).sendKeys(userName);
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-    }
-
     @AfterMethod
     public void tearDown(){
         stop();
     }
 
-    private void stop() {
-        driver.quit();
-    }
-
-    protected void submitNewGroup() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    protected void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
-    }
-
-    protected void createNewGroup() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    protected void goToGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    protected void checkNewContactAdded() {
-        assertEquals("Information entered into address book.\nadd next or return to home page.", driver.findElement(By.className("msgbox")).getText());
-    }
-
-    protected void submitNewContact() {
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-    }
-
-    protected void fillNewContactForm(ContactData contactData) {
-        driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-        driver.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
-        driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-        driver.findElement(By.name("mobile")).sendKeys(contactData.getPhoneNumber());
-        driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    protected void goToNewContactPage() {
-        driver.findElement(By.xpath("//a[text()='add new']")).click();
-    }
-
-    protected void checkDeletedSuccessfully() {
-        assertEquals("Group has been removed.\nreturn to the group page", driver.findElement(By.className("msgbox")).getText());
-    }
-
-    protected void clickDeleteButton() {
-        driver.findElement(By.name("delete")).click();
-    }
-
-    protected void clickFirstCheckboxInList() {
-        driver.findElement(By.xpath("//input[@type='checkbox']")).click();
-    }
 }
