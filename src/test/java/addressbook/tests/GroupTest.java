fb5b284aa3c125b256a170573d051ac869c65c6f
Alex Lobov <alobov@issart.com>
2022-07-21 15:47:21 +0700

    Добавил лямба выражение, которое вычисляет максмальный id для группы

--- src/test/java/addressbook/tests/GroupTest.java
+++ src/test/java/addressbook/tests/GroupTest.java
@@ -4,6 +4,7 @@ import addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,14 +20,8 @@ public class GroupTest extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        int max = 0;
-        for (GroupData g : after){
-            if (g.getId() > max){
-                max = g.getId();
-            }
-        }
-        group.setId(max);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
 
