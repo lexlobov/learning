4ba2b7050f44d09828e6ab8eaf8dd48ba2de115d
Alex Lobov <alobov@issart.com>
2022-09-06 15:37:31 +0700

    Добавил генератор групп

--- src/test/java/addressbook/generators/GroupDataGenerator.java
+++ src/test/java/addressbook/generators/GroupDataGenerator.java
@@ -1,4 +1,60 @@
 package addressbook.generators;
 
+import addressbook.model.GroupData;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupDataGenerator {
+
+    @Parameter(names = "-c", description =
+    "Group count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander jCommander = new JCommander().newBuilder().addObject(generator).build();
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException e){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<GroupData> groups = generateGroups(count);
+        save(groups, new File(file));
+    }
+
+    private void save(List<GroupData> groups, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (GroupData group : groups){
+            writer.write(String.format("%s;%S;%s", group.getGroupName(), group.getGroupHeader(), group.getGroupFooter()));
+        }
+        writer.close();
+    }
+
+    private List<GroupData> generateGroups(int count) {
+        List<GroupData> groups = new ArrayList<>();
+        for (int i= 0; i<count; i++){
+            groups.add(new GroupData()
+                    .withName(String.format("tst %s", i))
+                    .withHeader(String.format("Header %s", i))
+                    .withFooter(String.format("Footer %s", i)));
+        }
+        return groups;
+    }
 }
