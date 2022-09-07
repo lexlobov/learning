3b74cf15b08fd8a1c2675754669f350bf44bff70
Alex Lobov <alobov@issart.com>
2022-09-07 13:35:23 +0700

    Добавил параметризацию в тест создания групп

--- src/test/java/addressbook/tests/GroupCreateTest.java
+++ src/test/java/addressbook/tests/GroupCreateTest.java
@@ -2,18 +2,31 @@ package addressbook.tests;
 
 import addressbook.model.GroupData;
 import addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreateTest extends TestBase {
 
-    @Test
-    public void groupTest(){
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 3")});
+        list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 2")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void groupTest(GroupData group){
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         app.group().returnToGroupPage();
         Groups after = app.group().all();
