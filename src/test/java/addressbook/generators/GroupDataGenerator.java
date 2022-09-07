76eeafff955b873942460a9a70ba90676ec81f68
Alex Lobov <alobov@issart.com>
2022-09-07 15:17:50 +0700

    Добавил чтение тестовых данных из файла xml для параметризованного теста на создание групп, пока что не работает по неизвестным причинам

--- src/test/java/addressbook/generators/GroupDataGenerator.java
+++ src/test/java/addressbook/generators/GroupDataGenerator.java
@@ -4,6 +4,7 @@ import addressbook.model.GroupData;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,6 +22,10 @@ public class GroupDataGenerator {
     @Parameter(names = "-f", description = "Target file")
     public String file;
 
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
+
     public static void main(String[] args) throws IOException {
 
         GroupDataGenerator generator = new GroupDataGenerator();
@@ -36,10 +41,29 @@ public class GroupDataGenerator {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
-        save(groups, new File(file));
+        if (format.equals("csv")){
+            saveCsv(groups, new File(file));
+        } else if (format.equals("xml")){
+            saveXml(groups, new File(file));
+        } else {
+            System.out.println("Unrecognized format, enter csv or xml");
+        }
+
+
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveXml(List<GroupData> groups, File file) throws IOException {
+        XStream xstream = new XStream();
+        //xstream.alias("group", GroupData.class);
+        xstream.processAnnotations(GroupData.class);
+        String xml = xstream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+
+    }
+
+    private void saveCsv(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData group : groups){
             writer.write(String.format("%s;%S;%s\n", group.getGroupName(), group.getGroupHeader(), group.getGroupFooter()));
--- src/test/java/addressbook/tests/GroupCreateTest.java
+++ src/test/java/addressbook/tests/GroupCreateTest.java
@@ -2,13 +2,14 @@ package addressbook.tests;
 
 import addressbook.model.GroupData;
 import addressbook.model.Groups;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,18 +18,27 @@ public class GroupCreateTest extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
+//        List<Object[]> list = new ArrayList<Object[]>();
 //        list.add(new Object[] {new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
 //        list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 3")});
 //        list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 2")});
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+//        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
+        String xml = "";
         String line = reader.readLine();
         while (line != null){
-            String[] split = line.split(";");
-            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+//            String[] split = line.split(";");
+//            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
+
+        XStream xStream = new XStream();
+        xStream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+
     }
 
     @Test(dataProvider = "validGroups")
